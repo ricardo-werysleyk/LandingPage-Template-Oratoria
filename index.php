cb38f4468bc830a1a973d27a6adfa9f6c6a98255
Ricardo Werysleyk <werysleyk.ufrn@gmail.com>
2020-06-29 18:09:17 -0300

    First-Commit

---
+++ index.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="shortcut icon" href="assets/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
+
+    <title>Curso de Oratória</title>
+</head>
+<body>
+
+    <header>
+
+        <div class="logo">
+            <p>Logo-Marca</p>
+        </div>
+
+        <div class="container">
+            <h1>Curso de Oratória</h1>
+            <h3>Seja um orador inabalável com o curso mais completo do Brasil!</h3>
+        </div>
+    </header>
+
+    <section class="content">
+
+        <div class="descricao">
+
+            <video src="assets/teaser-placeholder.mp4" type="video/mp4" controls></video>
+
+            <ul>
+                <li><i class="fas fa-arrow-circle-right"></i><p>Descrição 1 Lorem ipsum dolor sit amet consectetur adipisicing elit.</p></li>
+                <li><i class="fas fa-arrow-circle-right"></i><p>Descrição 2 Lorem ipsum dolor sit amet consectetur adipisicing elit.</p></li>
+                <li><i class="fas fa-arrow-circle-right"></i><p>Descrição 3 Lorem ipsum dolor sit amet consectetur adipisicing elit.</p></li>
+                <li><i class="fas fa-arrow-circle-right"></i><p>Descrição 4 Lorem ipsum dolor sit amet consectetur adipisicing elit.</p></li>
+                <li><i class="fas fa-arrow-circle-right"></i><p>Descrição 5 Lorem ipsum dolor sit amet consectetur adipisicing elit.</p></li>
+            </ul>
+
+        </div>
+
+        <div class="conversao">
+
+            <form action="">
+
+                <div class="form-wraper">
+                    <input type="text" name="nome" id="nome" required>
+                    <label for="nome">Nome completo</label>
+                    <i class="fas fa-user"></i>
+                </div>
+
+                <div class="form-wraper">
+                    <input type="text" name="telefone" id="telefone" required>
+                    <label for="telefone">Telefone/Celular</label>
+                    <i class="fas fa-phone-square"></i>
+                </div>
+
+                <div class="form-wraper">
+                    <input type="email" name="email" id="email" required>
+                    <label for="email">E-mail</label>
+                    <i class="fas fa-envelope"></i>
+                </div>
+
+                <button type="submit">ADQUIRA AGORA!</button>
+
+            </form>
+
+        </div>
+
+</section>
+
+<section class="depoimentos-container">
+
+    <div class="container">
+
+        <div id="title-depoimentos">
+            <h1>DEPOIMENTOS</h1>
+        </div>
+
+        <div class="depoimentos">
+
+            <div class="depoimento-single">
+                <div class="perfil-depoimento">
+                    <img src="assets/perfil-placeholder.png" alt="">
+                    <h3>Davi Jhon</h3>
+                </div>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure eaque inventore enim possimus quo quas ut quisquam, voluptates, voluptatibus, maxime laborum molestiae reiciendis aspernatur beatae nisi recusandae hic. Incidunt, velit!</p>
+            </div>
+            <div class="depoimento-single">
+                <div class="perfil-depoimento">
+                    <img src="assets/perfil-placeholder.png" alt="">
+                    <h3>Davi Jhon</h3>
+                </div>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure eaque inventore enim possimus quo quas ut quisquam, voluptates, voluptatibus, maxime laborum molestiae reiciendis aspernatur beatae nisi recusandae hic. Incidunt, velit!</p>
+            </div>
+            <div class="depoimento-single">
+                <div class="perfil-depoimento">
+                    <img src="assets/perfil-placeholder.png" alt="">
+                    <h3>Davi Jhon</h3>
+                </div>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure eaque inventore enim possimus quo quas ut quisquam, voluptates, voluptatibus, maxime laborum molestiae reiciendis aspernatur beatae nisi recusandae hic. Incidunt, velit!</p>
+            </div>
+            <div class="depoimento-single">
+                <div class="perfil-depoimento">
+                    <img src="assets/perfil-placeholder.png" alt="">
+                    <h3>Davi Jhon</h3>
+                </div>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure eaque inventore enim possimus quo quas ut quisquam, voluptates, voluptatibus, maxime laborum molestiae reiciendis aspernatur beatae nisi recusandae hic. Incidunt, velit!</p>
+            </div>
+            <div class="depoimento-single">
+                <div class="perfil-depoimento">
+                    <img src="assets/perfil-placeholder.png" alt="">
+                    <h3>Davi Jhon</h3>
+                </div>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure eaque inventore enim possimus quo quas ut quisquam, voluptates, voluptatibus, maxime laborum molestiae reiciendis aspernatur beatae nisi recusandae hic. Incidunt, velit!</p>
+            </div>
+
+        </div>
+
+    </div>
+
+</section>
+    
+</body>
+</html>
